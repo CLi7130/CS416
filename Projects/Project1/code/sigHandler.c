613cf50104471f670d2a5cee0b36c2e1816886e0
Craig Li <you@example.com>
2021-10-12 16:54:02 -0400

    P2 upload

--- Projects/Project1/code/sigHandler.c
+++ Projects/Project1/code/sigHandler.c
@@ -11,16 +11,27 @@ void floating_point_exception_handler(int signum) {
 	printf("I am slain!\n");
 
 	/* Do your tricks here */
-
+    
     char* stackPointer = ((char*) &signum) + 0xCC;
     *stackPointer += 0x3;
 
     /*
         can also be done using an int* via:
-        int* stackPointer = &signum + (0xCC / 4);
+    
+        int* stackPointer = &signum + (0xCC / 0x4);
         *stackPointer += 0x3;
-    */
 
+        An additional note, modifying the program counter value by 0x3 causes the local variable values at the end of the function to change to:
+            x = 5
+            y = 0
+            z = 5
+        due to the fact that the exact command causing the sigFPE is the idivl command (3 bytes in length), but that idivl command is part of the division as a whole, which involves multiple assembly commands of moving values around through registers (7 bytes in length). By skipping only the idivl command, but not the entire division as a whole, we move the value of %eax (which at this point is the same value as x, located at -0xc(%rbp) into -0x4(%rbp), which is where the variable z is stored, thus changing the value of z to 5.  
+        
+        The assignment writeup is unclear as to whether this matters.  However, if we were to modify the change to the program counter to 0x6 instead of 0x3, we skip the move of x into z, leaving the variables unchanged when we exit the program.
+
+        So, the exact length of the bad instruction is 0x3, but to leave all variables unchanged, we would have to increment by 0x6.
+    
+    */
 }
 
 int main() {
