fc8d95df958201afc3652725275c4cab2d197474
cli7130 <cli7130@gmail.com>
2021-12-13 01:56:06 -0500

    updates to: get_avail_ino() get_avail_blkno() readi()

--- Projects/Project4/code/tfs.c
+++ Projects/Project4/code/tfs.c
@@ -11,6 +11,7 @@
 
 #define FUSE_USE_VERSION 26
 
+
 #include <fuse.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -28,36 +29,125 @@
 
 char diskfile_path[PATH_MAX];
 
-bitmap_t ibmap = NULL;
-bitmap_t dbmap = NULL;
-
-struct superblock *superblock;
-struct inode *inodes[MAX_INUM+2];
-struct dirent *dirent;
-
-
 // Declare your in-memory data structures here
+
+struct superblock* SBLOCK;
+bitmap_t INODE_BMAP;
+bitmap_t DBLOCK_BMAP;
+#define ERROR_VALUE -1
+#define DEBUG 0
+
+int SUPERBLOCK_BLOCK_SIZE;
+int INODE_BITMAP_BLOCK_SIZE;
+int DBLOCK_BITMAP_BLOCK_SIZE;
+int INODE_TABLE_BLOCK_SIZE;
+int BLOCK_SIZE_IN_CHARS;
+int INODES_PER_BLOCK;
+int DIRENTS_PER_BLOCK;
+
+/**
+ * Helper function to initialize memory for superblock, bitmaps, 
+ * and other useful global values.
+ */
+int init_FS_globals(){
+
+    if(DEBUG){
+        printf("IN init_FS_GLOBALS\n");
+    }
+
+    SUPERBLOCK_BLOCK_SIZE = ceil((double) 
+                            sizeof(struct superblock) / BLOCK_SIZE);
+    INODES_PER_BLOCK = floor((double) BLOCK_SIZE / sizeof(struct inode));
+    DIRENTS_PER_BLOCK = floor((double) BLOCK_SIZE / sizeof(struct dirent));
+    INODE_BITMAP_BLOCK_SIZE = ceil((double) (MAX_INUM / 8) / BLOCK_SIZE);
+    DBLOCK_BITMAP_BLOCK_SIZE = ceil((double) (MAX_DNUM / 8) / BLOCK_SIZE);
+    INODE_TABLE_BLOCK_SIZE = ceil(MAX_INUM / INODES_PER_BLOCK);
+    BLOCK_SIZE_IN_CHARS = ceil(BLOCK_SIZE / 8);
+
+    SBLOCK = calloc(sizeof(struct superblock), sizeof(struct superblock));
+    check_SBLOCK();
+
+    INODE_BMAP = (bitmap_t) malloc(MAX_INUM / 8);
+    DBLOCK_BMAP = (bitmap_t) malloc(MAX_DNUM / 8);
+
+    if(INODE_BMAP == NULL || DBLOCK_BMAP == NULL){
+
+    }
+
+}
+/**
+ * This is for error checking, just checks if the superblock
+ * is initialized, and exits the program if it isn't. Used to cut down
+ * redundancy in code.
+ */
+int check_SBLOCK(){
+
+    if(SBLOCK == NULL){
+        perror("SUPERBLOCK NOT INITIALIZED\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
 
 /* 
  * Get available inode number from bitmap
  */
 int get_avail_ino() {
+
+    if(DEBUG){
+        printf("in get_avail_ino\n");
+    }
+
+    //check superblock is initialized
+    check_SBLOCK();
 	
 	// Step 1: Read inode bitmap from disk
-	//superblock->i_bitmap_blk is the start address of inode bitmap
-	bio_read(superblock->i_bitmap_blk, ibmap);
+    void* inode_buffer = (void*) malloc(BLOCK_SIZE);
+    bio_read(SBLOCK->i_bitmap_blk, inode_buffer);
+
+    //check bitmap copied into inode_buffer
+    if(inode_buffer == NULL){
+        perror("INODE BITMAP NOT READ FROM DISK\n");
+        exit(EXIT_FAILURE);
+    }
+    //copy inode bitmap from inode_buffer into global
+    memcpy(INODE_BMAP, inode_buffer, MAX_INUM / 8);
 
 	// Step 2: Traverse inode bitmap to find an available slot
-	for(int i = 0; i < MAX_INUM; i++){
-		if(get_bitmap(ibmap, i) == 0){
-			// Step 3: Update inode bitmap and write to disk
-			set_bitmap(ibmap, i);
-			bio_write(superblock->i_bitmap_blk, ibmap);
-			return i;
-		}
-	}
 
-	return 0;
+    int inode_num = -1;
+    //iterate over inodes
+    while(inode_num < MAX_INUM){
+        inode_num++;
+        //bitmap is available/unused if it has 0 value
+        if(get_bitmap(INODE_BMAP, inode_num) == 0){
+            break;
+        } 
+    }
+    //no available inodes?
+    if(inode_num == ERROR_VALUE){
+        perror("NO AVAILABLE INODES IN TABLE\n");
+        exit(EXIT_FAILURE);
+    }
+
+	// Step 3: Update inode bitmap and write to disk
+
+    //update inode bitmap
+    set_bitmap(INODE_BMAP, inode_num);
+
+    //write to disk
+    bio_write(SBLOCK->i_bitmap_blk, (void*) INODE_BMAP);
+
+    int iterator = 0;
+    while(iterator < INODE_BITMAP_BLOCK_SIZE){
+        //write to disk
+        bio_write(SBLOCK->i_bitmap_blk + iterator,
+                  INODE_BMAP[iterator * BLOCK_SIZE_IN_CHARS]);
+    }
+
+    //free(inode_buffer);
+	return inode_num;
 }
 
 /* 
@@ -65,21 +155,52 @@ int get_avail_ino() {
  */
 int get_avail_blkno() {
 
+    if(DEBUG){
+        printf("IN GET_AVAIL_BLKNO\n");
+    }
+
 	// Step 1: Read data block bitmap from disk
-	//superblock->d_bitmap_blk is the start address of data block bitmap
-	bio_read(superblock->d_bitmap_blk, dbmap);
+	//SBLOCK->d_bitmap_blk is the start address of data block bitmap
 
-	// Step 2: Traverse data block bitmap to find an available slot
-	for(int i = 0; i < MAX_DNUM; i++){
-		if(get_bitmap(dbmap, i) == 0){
-			// Step 3: Update data block bitmap and write to disk
-			set_bitmap(dbmap, i);
-			bio_write(superblock->d_bitmap_blk, dbmap);
-			return i;
-		}
-	}
+    check_SBLOCK();
 
-	return 0;
+    void* dblock_buffer = (void*) malloc(BLOCK_SIZE);
+    bio_read(SBLOCK->d_bitmap_blk, dblock_buffer);
+
+    //check bitmap copied into buffer
+    if(dblock_buffer == NULL){
+        perror("DBLOCK BITMAP NOT READ FROM DISK\n");
+        exit(EXIT_FAILURE);
+    }
+    //copy inode bitmap from buffer into global
+    memcpy(DBLOCK_BMAP, dblock_buffer, MAX_DNUM / 8);
+
+	
+    // Step 2: Traverse data block bitmap to find an available slot
+
+    int dblock_index = -1;
+
+    while(dblock_index < MAX_DNUM){
+        dblock_index++;
+        //get available data block from bitmap
+        if(get_bitmap(DBLOCK_BMAP, dblock_index) == 0){
+            break;
+        }
+    }
+
+    //check to see we found an empty data block
+    if(dblock_index == ERROR_VALUE){
+        perror("NO AVAILABLE DATA BLOCKS\n");
+        exit(EXIT_FAILURE);
+    }
+
+	// Step 3: Update data block bitmap and write to disk
+
+    set_bitmap(DBLOCK_BMAP, dblock_index);
+    bio_write(SBLOCK->d_bitmap_blk, (void*) DBLOCK_BMAP);
+    free(dblock_buffer);
+
+	return dblock_index;
 }
 
 /* 
@@ -88,56 +209,67 @@ int get_avail_blkno() {
 int readi(uint16_t ino, struct inode *inode) {
 	//uint16_t is an unsigned 16 bit integer
 
+    if(DEBUG){
+        printf("IN READI\n");
+    }
+
   	// Step 1: Get the inode's on-disk block number
-	// i_start_blk = start address of inode region
-	int offset = BLOCK_SIZE/sizeof(struct inode);
-	int iBlockNum = superblock->i_start_blk + ino/offset;
-	struct inode *iBlock = malloc(BLOCK_SIZE);
-	int status = bio_read(iBlockNum, iBlock);
+
+    check_SBLOCK();
+
+    // i_start_blk = start address of inode region
+    int block_num = SBLOCK->i_start_blk + (ino / INODES_PER_BLOCK);
 
   	// Step 2: Get offset of the inode in the inode on-disk block
-	int iOffsetInBlk = ino % offset;
+
+    int offset = ino % INODES_PER_BLOCK;
 
   	// Step 3: Read the block from disk and then copy into inode structure
-	inodes[ino - 1]->ino = iBlock[iOffsetInBlk].ino;
-	inodes[ino - 1]->valid = iBlock[iOffsetInBlk].valid;
-	inodes[ino - 1]->size = iBlock[iOffsetInBlk].size;
-	inodes[ino - 1]->type = iBlock[iOffsetInBlk].type;
-	inodes[ino - 1]->link = iBlock[iOffsetInBlk].link;
-	
-	for(int i = 0; i < DIRECT_PTRS;i++){
 
-		inodes[ino - 1]->direct_ptr[i] = iBlock[iOffsetInBlk].direct_ptr[i];
-		if(i < INDIRECT_PTRS){
+    struct inode* buffer_block = (void*) malloc(BLOCK_SIZE);
 
-			inodes[ino - 1]->indirect_ptr[i] = iBlock[iOffsetInBlk].indirect_ptr[i];
-			
-		}
+    if(buffer_block == NULL){
+        perror("READI: DID NOT ALLOCATE MEMORY FOR BUFFER_BLOCK\n");
+        exit(EXIT_FAILURE);
+    }
 
-	}
-	inodes[ino - 1]->vstat.st_dev = iBlock[iOffsetInBlk].vstat.st_dev;
-	inodes[ino - 1]->vstat.st_ino = iBlock[iOffsetInBlk].vstat.st_ino;
-	inodes[ino - 1]->vstat.st_mode = iBlock[iOffsetInBlk].vstat.st_mode;
-	inodes[ino - 1]->vstat.st_nlink = iBlock[iOffsetInBlk].vstat.st_nlink;
-	inodes[ino - 1]->vstat.st_uid = iBlock[iOffsetInBlk].vstat.st_uid;
-	inodes[ino - 1]->vstat.st_gid = iBlock[iOffsetInBlk].vstat.st_gid;
-	inodes[ino - 1]->vstat.st_atime = iBlock[iOffsetInBlk].vstat.st_atime;
-	inodes[ino - 1]->vstat.st_mtime = iBlock[iOffsetInBlk].vstat.st_mtime;
+    //read from disk
+    if(bio_read(block_num, buffer_block) == NULL){
+        perror("FAILURE TO READ INODE FROM INODE TABLE\n");
+        exit(EXIT_FAILURE);
+    }
 
-	free(iBlock);
-	iBlock = NULL;
-	
-	return status;
+    struct inode* inode_block = (struct inode*) buffer_block;
+
+    if(DEBUG){
+        printf("READING INTO INODE:\n"
+                + "ino: %d, "
+                + "block number: %d "
+                + "offset: %d\n",
+                ino,
+                block_num,
+                offset);
+    }
+
+    //change input inode's pointer to block's data
+    //same effect as copying all fields into inode
+    *inode = inode_block[offset];
+    free(buffer_block);
+
+	return 1;
 }
 
 
 int writei(uint16_t ino, struct inode *inode) {
 
+    if(DEBUG){
+        printf("in writei\n");
+    }
+    check_SBLOCK();
 	// Step 1: Get the block number where this inode resides on disk
-	int offset = BLOCK_SIZE/sizeof(struct inode);
-	int iBlockNum = superblock->i_start_blk + ino/offset;
-	struct inode *iBlock = malloc(BLOCK_SIZE);
-	int status = bio_read(iBlockNum, iBlock);
+
+    int offset = ino % INODES_PER_BLOCK;
+    int block_num = SBLOCK->i_start_blk + (ino / INODES_PER_BLOCK);
 
 	// Step 2: Get the offset in the block where this inode resides on disk
 	int iOffsetInBlk = ino % offset;
@@ -182,7 +314,37 @@ int writei(uint16_t ino, struct inode *inode) {
  */
 int dir_find(uint16_t ino, const char *fname, size_t name_len, struct dirent *dirent) {
 
+    /*
+        struct dirent {
+            uint16_t ino;					inode number of the directory entry 
+            uint16_t valid;					validity of the directory entry 
+            char name[252];					name of the directory entry 
+        };
+    */
+
   // Step 1: Call readi() to get the inode using ino (inode number of current directory)
+
+  //have to find where directory is in file system
+  //copy filePath from args
+  char* filePath = (char*) malloc(sizeof(fname));
+  strcpy(fname, filePath);
+  //split via strtok?
+
+  uint16_t inodeStatus; //see whether inode is valid? have to find inode in directory
+
+  if(inodeStatus < 0){//error checking
+      perror("dir_find failed\n");
+      return ERROR_VALUE;
+  }
+
+  
+
+
+  //uint16_t inodeStatus =
+
+
+
+
 
   // Step 2: Get data block of current directory from inode
 
@@ -235,6 +397,8 @@ int get_node_by_path(const char *path, uint16_t ino, struct inode *inode) {
  * Make file system
  */
 int tfs_mkfs() {
+
+    init_FS_globals();
 
 	// Call dev_init() to initialize (Create) Diskfile
 
@@ -473,4 +637,3 @@ int main(int argc, char *argv[]) {
 
 	return fuse_stat;
 }
-
